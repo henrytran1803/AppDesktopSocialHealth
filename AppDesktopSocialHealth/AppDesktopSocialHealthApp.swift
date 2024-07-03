a67fbc727296a0db54a37709f13bb8af46884cc8
Trần Việt Anh <tranvietanh1803@gmail.com>
2024-07-03 21:41:58 +0700

    Initial Commit

---
+++ AppDesktopSocialHealth/AppDesktopSocialHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDesktopSocialHealthApp.swift
+//  AppDesktopSocialHealth
+//
+//  Created by Tran Viet Anh on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AppDesktopSocialHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
